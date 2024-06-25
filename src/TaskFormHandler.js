ceb1f9951d050ae0281e73cf7aa7f099ffeb94b7
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2024-06-25 19:23:32 +0200

    working on task Items

--- src/TaskFormHandler.js
+++ src/TaskFormHandler.js
@@ -3,8 +3,64 @@ const taskForm = document.getElementById('task-form');
 const addTaskBtn = document.getElementById('add-task');
 const taskFormSubmit = document.getElementById('task-form-submit');
 const taskFormList = document.querySelector('.task-form-list');
+import { taskManager } from "./TaskProjectCreator";
+
+function createTaskElement(item) {
+    const listTodo = document.createElement('li');
+    listTodo.setAttribute("task-project", item.id);
+    listTodo.classList.add('task-item');
+ 
+    const taskItemCheckboxInput = document.createElement('input');
+    taskItemCheckboxInput.setAttribute('type', 'checkbox');
+    taskItemCheckboxInput.id = "task-item-checkbox";
+ 
+    const listDetails = document.createElement('div');
+    listDetails.classList.add('list-details');
+ 
+    const taskItemTitle = document.createElement('h3');
+    taskItemTitle.classList.add('task-title');
+    taskItemTitle.textContent = item.title;
+ 
+    const taskItemDetails = document.createElement('p');
+    taskItemDetails.textContent = item.details;
+    taskItemDetails.classList.add('task-details');
+ 
+    listDetails.append(taskItemTitle, taskItemDetails);
+
+    const taskItemDate = document.createElement('div');
+    taskItemDate.textContent = item.date;
+    taskItemDate.classList.add('task-date');
+
+    taskItemCheckboxInput.addEventListener('change', () => {
+        if(taskItemCheckboxInput.checked) {
+            console.log('checkboc je chekcan');
+            listDetails.classList.add('fadeColor');
+        } else {
+            console.log('checkbox je uncheckan');
+            listDetails.classList.remove('fadeColor');
+        }
+    })
+ 
+    listTodo.append(taskItemCheckboxInput,listDetails, taskItemDate);
+ 
+    return listTodo;
+    
+ }
 
 
+function renderTaskItem() {
+    const inputTitle = document.getElementById('title').value;
+    const inputDetails = document.getElementById('details').value;
+    const inputDate = document.getElementById('date').value;
+
+    const tasksArray = taskManager.createTask(inputTitle, inputDetails, inputDate);
+
+    tasksArray.forEach((taskItem) => {
+        taskFormList.appendChild(createTaskElement(taskItem));
+    })
+
+    console.log(inputTitle, inputDetails, inputDate);
+}
 
 function renderTodoItemDetails(event) {
     event.preventDefault();
@@ -25,16 +81,19 @@ function handleTaskFormSubmission(event) {
     event.stopPropagation();
     event.stopImmediatePropagation();
 
-    const inputTitle = document.getElementById('title').value;
-    const inputDetails = document.getElementById('details').value;
-    const inputDate = document.getElementById('date').value;
+    taskFormList.innerHTML = "";
 
-    console.log(inputTitle, inputDetails, inputDate);
+    renderTaskItem();
+    clearInputFields();
+    
 }
 
-function createTaskElement(title, details, Date) {
-   const listItem = document.createElement('li');
-   
+
+function clearInputFields() {
+    const inputFields = document.querySelectorAll('.task-input-field');
+    inputFields.forEach((inputField) => {
+        inputField.value = "";
+    })
 }
 
 taskFormSubmit.addEventListener('click', handleTaskFormSubmission);
--- src/TaskProjectCreator.js
+++ src/TaskProjectCreator.js
@@ -3,8 +3,8 @@ class TaskManager {
         this.tasks = [];
     }
 
-    createTask(name) {
-        const newTask = new TaskProject(name);
+    createTask(title, details, date) {
+        const newTask = new TaskProject(title, details, date);
         this.tasks.push(newTask);
         this.updateTaskIDs();
         return this.tasks;
@@ -15,13 +15,33 @@ class TaskManager {
     }
 
     deleteTaskById(id) {
-        
+        if(this.tasks[id]) {
+            console.log(`ID of item that will be deleted ${id}`);
+            this.tasks.splice(id, 1);
+            this.updateTaskIDs();
+        } else {
+            console.error(`Task with ID: ${id} not found`);
+        }
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+
+    updateTaskIDs() {
+        this.tasks = this.tasks.map((item, index) => (item.id = index, item));
     }
 }
 
 class TaskProject {
-    constructor(name) {
-        this.id = id;
-        this.name = name;
+    constructor(title, details, date) {
+        this.id;
+        this.title = title;
+        this.details = details;
+        this.date = date;
     }
 }
+
+const taskManager = new TaskManager();
+
+export {taskManager};
